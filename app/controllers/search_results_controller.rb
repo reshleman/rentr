a544690bcaebd601376597dafe61d54e3dfaeb2f
Nicholas Narh <nicknarh@gmail.com>
2014-08-13 15:00:34 -0400

    update search controller

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -2,6 +2,8 @@ class SearchResultsController < ApplicationController
   skip_before_action :require_login, only: [:show]
 
   def show
+    @city = params[:city]
+    @price = params[:price]
     @listings = Listing.search(params)
   end
 end
