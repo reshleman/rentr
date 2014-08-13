e570fe28189e09f993b9fa2ea9d9c421d718bdc6
Charles Massry <charlesdmassry@gmail.com>
2014-08-13 08:59:02 -0400

    satisfy the hound

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,7 +5,7 @@ class Reservation < ActiveRecord::Base
   validates :listing, presence: true
   validates :user, presence: true
   validates :start_date, presence: true,
-            date: { before_or_equal_to: Proc.new { :end_date } }
+                         date: { before_or_equal_to: Proc.new { :end_date } }
   validates :end_date, presence: true,
-            date: { after_or_equal_to: Proc.new { :start_date } }
+                       date: { after_or_equal_to: Proc.new { :start_date } }
 end
