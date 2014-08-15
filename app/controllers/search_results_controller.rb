be9a6b00ab5f0f833e46b76eae6db655c2df6de0
Nicholas Narh <nicknarh@gmail.com>
2014-08-15 11:18:54 -0400

    Search by Property Category
    * Update search logic in listing controller
    * Add room category to fields to forms
    * Update search results controller
    * The room category commit should be swapped with this commit
    https://trello.com/c/F9jyfw3A

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -6,6 +6,19 @@ class SearchResultsController < ApplicationController
     @price = params[:price]
     @accommodates = params[:accomodates]
     @property_categories = PropertyCategory.all
+    @room_categories = RoomCategory.all
     @listings = Listing.search(params)
+    @search_query = search_query
+  end
+
+  private
+
+  def search_query
+    [
+      params[:city],
+      params[:price],
+      params[:property_categories],
+      params[:room_categories]
+    ].select(&:present?).join(" ")
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -17,22 +17,42 @@ class Listing < ActiveRecord::Base
   SEARCH_RANGE = 0..99
 
   def self.search(search_params)
-    result_set = where("city ILIKE ?", "%#{search_params[:city]}%")
+    city(search_params[:city]).
+      price(search_params[:price]).
+      property_category(search_params[:property_category]).
+      room_category(search_params[:room_category])
+  end
 
-    if search_params[:price].present?
-      result_set = result_set.where("price <= ?", search_params[:price])
+  def self.city(city)
+    query_if_present(city) do |c|
+      where("city ILIKE ?", "%#{c}%")
     end
+  end
 
-    if search_params[:property_category].present?
-      result_set = result_set.
-        where("property_category_id = ?", search_params[:property_category])
+  def self.price(price)
+    query_if_present(price) do |p|
+      where("price <= ?", p)
     end
+  end
 
-    if search_params[:accommodates].present?
-      result_set = result_set.
-        where("accommodates <= ?", search_params[:accommodates])
+  def self.property_category(category)
+    query_if_present(category) do |ct|
+      where("property_category_id = ?", ct)
     end
-    result_set
+  end
+
+  def self.room_category(category)
+    query_if_present(category) do |cat|
+      where("room_category_id = ?", cat)
+    end
+  end
+
+  def self.query_if_present(term, &block)
+    if term.present?
+      block.call(term)
+    else
+      all
+    end
   end
 
   def make_available_on(date)
