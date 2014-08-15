8404c61b2cc373c9a875dd62e749bdca99e65552
Nicholas Narh <nicknarh@gmail.com>
2014-08-15 15:34:34 -0400

    refactor listing and form

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -7,5 +7,12 @@ class SearchResultsController < ApplicationController
     @property_categories = PropertyCategory.all
     @room_categories = RoomCategory.all
     @listings = Listing.search(params)
+    @search = search_query
+  end
+
+  private
+
+  def search_query
+    [params[:city], params[:price], params[:property_categories], params[:room_categories]].join(" ")
   end
 end
